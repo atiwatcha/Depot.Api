0cb15de4ef6f95d5a2b98f11f48bcd2b4bbb6a5f
atiwat.chananet <atiwat.chananet@gmail.com>
2018-08-15 22:24:38 +0700

    dotnet web

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Depot.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .Build();
+
+            //host.Run();
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+           .UseUrls("http://*:5000") //Add this line of code.
+            .Build();
+
+            host.Run();
+        }
+    }
+}
